66f6eabeccf139ba9b495907fb6645c1d8fd2a67
Péter Csizmadia <csizipeti0723@gmail.com>
2021-10-27 19:13:38 +0200

    custom logging format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,14 @@
 import { createLogger, format, transports } from "winston";
 
+const jsonLogFileFormat = format.combine(
+  format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  format.errors({ stack: true }),
+  format.json()
+);
+
 const logger = createLogger({
   level: "info",
-  format: format.combine(
-    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
+  format: jsonLogFileFormat,
   transports: [
     new transports.File({ filename: "error.log", level: "error", handleExceptions: true }),
     new transports.File({ filename: "combined.log", handleExceptions: true }),
@@ -15,10 +16,24 @@ const logger = createLogger({
 });
 
 if (process.env.NODE_ENV !== "production") {
+  const myFormat = format.printf(({ level, message, timestamp, stack, ...metadata }) => {
+    let msg = `\x1b[36m${timestamp}\x1b[0m [${level}] : ${message}`;
+    if (stack) msg += `\n${stack}`;
+    if (metadata[Object.keys(metadata)[0]] !== undefined) msg += ` ${JSON.stringify(metadata)}`;
+    return msg;
+  });
+
   logger.add(
     new transports.Console({
-      format: format.cli(),
+      format: format.combine(
+        format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        format.errors({ stack: true }),
+        format.colorize(),
+        myFormat
+      ),
       handleExceptions: true,
+      stderrLevels: ["error"],
+      consoleWarnLevels: ["warn"],
     })
   );
 }
